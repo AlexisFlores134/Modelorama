a645d1ec32b0a17d2506db40484857d38b1d8134
Alexis <af973848@gmail.com>
2024-11-28 14:17:02 -0600

    ServerFinalCreo

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+Route::get('/api/docs', function () {
+    return view('l5-swagger::swagger-ui');
+});
 
 require __DIR__.'/auth.php';
